c9bc143ce80e3aa47e55704105b1cb079c7e34d7
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-04-01 21:35:44 +0530

    Add files via upload

---
+++ ListRowEdit/ListRowEdit/ListRowEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRowEditApp.swift
+//  ListRowEdit
+//
+//  Created by ganeshrajugalla on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ListRowEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
