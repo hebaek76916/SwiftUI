63259da17ca333706eacc16f90765c667f23b873
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-04-01 18:57:24 +0530

    Add files via upload

---
+++ StaticList/StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by ganeshrajugalla on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
