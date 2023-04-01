8bb8257f9a058dcf2afb52a57dacce6ec483517c
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-04-01 21:41:20 +0530

    Add files via upload

---
+++ MovingListRows/MovingListRows/MovingListRowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingListRowsApp.swift
+//  MovingListRows
+//
+//  Created by ganeshrajugalla on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovingListRowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
