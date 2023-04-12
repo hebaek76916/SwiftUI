bcfbb69c4f32db58c65e1a3c6c98f45c23d21b0f
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-04-13 00:48:26 +0530

    Add files via upload

---
+++ ProgressRings/ProgressRings/ProgressRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressRingsApp.swift
+//  ProgressRings
+//
+//  Created by Ganesh on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
