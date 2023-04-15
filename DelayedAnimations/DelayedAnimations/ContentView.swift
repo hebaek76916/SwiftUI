7652cf59b637667d829f961fb69dbc6b7b39aee2
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-04-15 15:50:28 +0530

    Add files via upload

---
+++ DelayedAnimations/DelayedAnimations/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  DelayedAnimations
+//
+//  Created by Ganesh on 15/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let duration = 1.0
+    @State private var change: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Rectangle()
+                .fill(.purple)
+                .offset(y: change ? -300 : 0)
+                .animation(.easeInOut(duration: duration).delay(0), value: change)
+                .scaleEffect(change ? 0.5 : 1)
+                .animation(.easeInOut(duration: duration), value: change)
+                .rotation3DEffect(change ? .degrees(45) : .degrees(0), axis: (x: 1, y: 0, z: 0))
+                .animation(.easeInOut(duration: duration).delay(2*duration), value: change)
+                .frame(width: 200,height: 200)
+            
+            Button {
+                change.toggle()
+            } label: {
+                Text("Animate")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.green)
+                    .cornerRadius(5)
+            }
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DelayedAnimations/DelayedAnimations/DelayedAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelayedAnimationsApp.swift
+//  DelayedAnimations
+//
+//  Created by Ganesh on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DelayedAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
